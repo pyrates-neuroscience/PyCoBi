035d979f472c2cc83b56a1218c7acc5c7f5130ad
Richert <richgast.90@gmail.com>
2023-11-14 12:10:56 -0600

    updated supported python version in setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("requirements.txt", "r", encoding="utf8") as fh:
 INSTALL_REQUIREMENTS = REQUIREMENTS
 
 CLASSIFIERS = ["Programming Language :: Python :: 3",
-               "Programming Language :: Python :: 3.6",
                "Programming Language :: Python :: 3.7",
                "Programming Language :: Python :: 3.8",
                "Programming Language :: Python :: 3.9",
+               "Programming Language :: Python :: 3.10",
                "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
                "Operating System :: OS Independent",
                "Development Status :: 3 - Alpha",
